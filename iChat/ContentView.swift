bb7cfbb0c0387b0e7b5502b9b72087d0dc5cf724
Anderson Menezes <anderson.stifip@gmail.com>
2022-07-07 22:12:31 -0300

    Initial Commit

---
+++ iChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iChat
+//
+//  Created by Anderson Menezes on 07/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iChat/iChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  iChatApp.swift
+//  iChat
+//
+//  Created by Anderson Menezes on 07/07/22.
+//
+
+import SwiftUI
+
+@main
+struct iChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
